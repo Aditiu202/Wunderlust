51a688376d9c7ea7a6ca660636c22c7ef2d235bc
Aditi Upadhyay <121815386+Aditiu202@users.noreply.github.com>
2023-12-10 12:24:25 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,1 +1,20 @@
+const MongooseURL = "mongodb://127.0.0.1:27017/wanderlust";
+async function main(){
+   await mongoose.connect(MongooseURL);
+}
 
+main().then(()=>{
+    console.log("connected to db");
+}) 
+.catch((err)=>{
+    console.log(err);
+})
+
+const initDB  = async()=>{
+await Listing.deleteMany({});
+await Listing.insertMany(initdata.data);
+console.log("data was initalised");
+}
+
+initDB();
+ 
